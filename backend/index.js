a927eeaa319f876991427d4a304e689d0b4bb450
Faizan Ali <151016446+faizan1236@users.noreply.github.com>
2023-12-22 01:00:41 -0800

    new changing

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,14 @@ const todo = require("./routes/Todo");
 const app = express();
 app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/todo", todo);
 const port = 4000;
 
 const DATABASE_URL = process.env.DATABASE_URL;
 connectDB(DATABASE_URL);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/todo", todo);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
